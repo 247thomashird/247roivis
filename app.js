46d4ad194ff322628d84fbe83850b7b9c5cad42b
Thomas C. Hird <thomas.hird@ca-THird-mac.local>
2015-08-13 13:00:22 -0700

    initial upload

---
+++ app.js
@@ -0,0 +1,43 @@
+google.load('visualization','1.0', {'packages' :['corechart', 'bar']});
+google.setOnLoadCallback(drawchart);
+var  products=['Voice Agent','IVR','Vivid Speech', 'Virtual Assistant','Mobile Chat', 'Web Chat'];
+var costbefore=[1,2,3,4,5,6];
+var costafter=[6,5,4,3,2,1];
+function drawchart(){
+	// var data = new google.visualization.arrayToDataTable([
+	// 	products,
+	// 	costbefore,
+	// 	costafter
+	// 	])
+	// var options = {
+	// 	width:640,
+	// 	height: 360,
+	// 	legend:{ position: 'bottom'},
+	// 	bar:{groupwidth:'75'},
+	// 	isStacked: true
+	// };
+	var data = google.visualization.arrayToDataTable([
+        ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+         'Western', 'Literature', { role: 'annotation' } ],
+        ['2010', 10, 24, 20, 32, 18, 5, ''],
+        ['2020', 16, 22, 23, 30, 16, 9, ''],
+        ['2030', 28, 19, 29, 30, 12, 13, '']
+      ]);
+
+      var options = {
+        width: 600,
+        height: 400,
+        legend: { position: 'top', maxLines: 3 },
+        bar: { groupWidth: '75%' },
+        bars: 'horizontal',
+        isStacked: true
+      };
+       var options_stacked = {
+          isStacked: true,
+          height: 300,
+          legend: {position: 'top', maxLines: 3},
+          hAxis: {minValue: 0}
+        };
+	var material = new google.charts.Bar(document.getElementById('chart1'));
+	material.draw(data,options);
+}

835e2ee414f05f550dd3a05802c36ff6df070eda
Thomas C. Hird <thomas.hird@ca-THird-mac.local>
2015-08-18 10:51:35 -0700

    v3

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 google.load('visualization','1.0', {'packages' :['corechart', 'bar']});
-//google.setOnLoadCallback(drawchart);
-var  products=['Voice Agent','IVR','Vivid Speech', 'Virtual Assistant','Mobile Chat', 'Web Chat'];
-var costbefore=[1,2,3,4,5,6];
-var costafter=[6,5,4,3,2,1];
+google.setOnLoadCallback(drawchart);
+var  products=['genre','Voice Agents','IVR','Vivid Speech', 'Virtual Assistant','Mobile Chat', 'Web Chat'];
+var costbefore=['before',1,2,3,4,5,6];
+var costafter=['after',6,5,4,3,2,1];
 
 var graphdef = {
 	categories: ['before','after'],
@@ -26,41 +26,33 @@ var graphdef = {
 	}
 };
 var chart = uv.chart('StackedBar',graphdef);
-//function drawchart(){
-	// var data = new google.visualization.arrayToDataTable([
-	// 	products,
-	// 	costbefore,
-	// 	costafter
-	// 	])
-	// var options = {
-	// 	width:640,
-	// 	height: 360,
-	// 	legend:{ position: 'bottom'},
-	// 	bar:{groupwidth:'75'},
-	// 	isStacked: true
-	// };
-// 	var data = google.visualization.arrayToDataTable([
-//         ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
-//          'Western', 'Literature', { role: 'annotation' } ],
-//         ['2010', 10, 24, 20, 32, 18, 5, ''],
-//         ['2020', 16, 22, 23, 30, 16, 9, ''],
-//         ['2030', 28, 19, 29, 30, 12, 13, '']
-//       ]);
-
-//       var options = {
-//         width: 600,
-//         height: 400,
-//         legend: { position: 'top', maxLines: 3 },
-//         bar: { groupWidth: '75%' },
-//         bars: 'horizontal',
-//         isStacked: true
-//       };
-//        var options_stacked = {
-//           isStacked: true,
-//           height: 300,
-//           legend: {position: 'top', maxLines: 3},
-//           hAxis: {minValue: 0}
-//         };
-// 	var material = new google.charts.Bar(document.getElementById('chart1'));
-// 	material.draw(data,options);
-// }
+function drawchart(){
+	var data = new google.visualization.arrayToDataTable([
+		products,
+		costbefore,
+		costafter
+		])
+	var options = {
+		width:990,
+		height: 360,
+		legend:{ position: 'bottom'},
+		bar:{groupwidth:'75'},
+		isStacked: true
+	};
+	var chart = new google.visualization.BarChart(document.getElementById('chart3'));
+	chart.draw(data,options);
+}
+console.log("loaded app.js");
+window.onload = function(){
+	$("#chart1").hide();
+	$("#chart2").hide();
+	resize();
+}
+$(window).resize(function(){
+	resize();
+});
+function resize(){
+	console.log("resize");
+	var height = $(window).height();
+	$("#settingsbar").height(height-55-50);
+}

3d3d24db00dc5b5f455a725730d4b2ed9f9635f9
Thomas C. Hird <thomas.hird@ca-THird-mac.local>
2015-08-24 16:36:29 -0700

    created new directory for v3

--- 3.0/app.js
+++ 3.0/app.js
@@ -1,4 +1,26 @@
+google.load('visualization','1.0', {'packages' :['corechart', 'bar']});
+google.setOnLoadCallback(drawchart);
+var  products=['genre','Voice Agents','IVR','Vivid Speech', 'Virtual Assistant','Mobile Chat', 'Web Chat'];
+var costbefore=['before',1,2,3,4,5,6];
+var costafter=['after',6,5,4,3,2,1];
 
+function drawchart(){
+	var data = new google.visualization.arrayToDataTable([
+		products,
+		costbefore,
+		costafter
+		])
+	var options = {
+		width:990,
+		height: 360,
+		legend:{ position: 'bottom'},
+		bar:{groupwidth:'75'},
+		isStacked: true
+	};
+	var chart = new google.visualization.BarChart(document.getElementById('chart3'));
+	chart.draw(data,options);
+}
+console.log("loaded app.js");
 window.onload = function(){
 	resize();
 }
@@ -6,8 +28,7 @@ $(window).resize(function(){
 	resize();
 });
 function resize(){
-	var height= $(window).height();
-	var width= $(window).width();
-	$("#settings").height(height-105);
-	console.log("height: "+height);
+	console.log("resize");
+	var height = $(window).height();
+	$("#settingsbar").height(height-55-50);
 }
